2ac6588c89e99a4d5487edc34b8ab367b6dc3732
Numulix <jbabic999@gmail.com>
2025-03-29 23:34:05 +0100

    feat: add about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header.component";
 import { HeroComponent } from "./hero/hero.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, HeroComponent],
+  imports: [Header, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/utils/aboutItems.ts
@@ -0,0 +1,19 @@
+interface AboutItem {
+    label: string;
+    number: number;
+}
+
+export const aboutItems: AboutItem[] = [
+    {
+        label: 'Projects done',
+        number: 10
+    },
+    {
+        label: 'Years of experience',
+        number: 3
+    },
+    {
+        label: 'Pokemon caught',
+        number: 580
+    }
+]
