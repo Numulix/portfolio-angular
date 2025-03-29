7af27891e0c76d6e1ebdab35910129a4d1c78b1a
Numulix <jbabic999@gmail.com>
2025-03-29 17:11:10 +0100

    feat: start on hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header.component";
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header],
+  imports: [Header, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
