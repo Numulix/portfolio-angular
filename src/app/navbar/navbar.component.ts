44160d751fc9eeae024380fcbce4189fc5a231d4
Numulix <jbabic999@gmail.com>
2025-04-01 10:48:57 +0200

    fix: fix race condition on navbar active box rendering

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, Inject, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Inject, Input, ViewChild } from '@angular/core';
 import { navItems } from '../../consts/navItems';
 import { DOCUMENT } from '@angular/common';
 
@@ -9,14 +9,14 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements AfterViewInit, AfterViewChecked {
+export class NavbarComponent implements AfterViewInit {
   @ViewChild('activeBox') activeBox!: ElementRef;
 
   @Input() navOpen: boolean = false;
 
   @HostListener('window:resize')
   onResize() {
-    this._initActiveBox();
+    setTimeout(() => this._initActiveBox());
   }
 
   currentActiveEl: any = null;
@@ -30,28 +30,27 @@ export class NavbarComponent implements AfterViewInit, AfterViewChecked {
       this.activeBox.nativeElement.style.left = this.currentActiveEl.offsetLeft + 'px';
       this.activeBox.nativeElement.style.width = this.currentActiveEl.offsetWidth + 'px';
       this.activeBox.nativeElement.style.height = this.currentActiveEl.offsetHeight + 'px';
+      this.activeBox.nativeElement.style.opacity = 1;
+    } else {
+      this.activeBox.nativeElement.style.opacity = 0;
     }
   }
 
   constructor (@Inject(DOCUMENT) private _document: Document) {}
- 
-  ngAfterViewInit(): void {
-    this.currentActiveEl = this._document.getElementsByClassName("active")[1] || this._document.getElementsByClassName("active")[0];
-    this.lastActiveEl = this._document.getElementsByClassName("active")[1] || this._document.getElementsByClassName("active")[0];    
-    this._initActiveBox();
-  }
 
-  ngAfterViewChecked(): void {
-    if (!this.currentActiveEl) {
-      this.currentActiveEl = this._document.getElementsByClassName("active")[1];
-      this.lastActiveEl = this._document.getElementsByClassName("active")[1];
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.currentActiveEl = this._document.getElementsByClassName("active")[1] || this._document.getElementsByClassName("active")[0];
+      this.lastActiveEl = this.currentActiveEl;
       this._initActiveBox();
-    }
+    });
   }
 
   activeCurrentLink(target: EventTarget | null): void {
-    if (target instanceof HTMLAnchorElement) {
-      this.lastActiveEl.classList.remove('active');
+    if (target instanceof HTMLAnchorElement && target !== this.lastActiveEl) {
+      if(this.lastActiveEl) {
+         this.lastActiveEl.classList.remove('active');
+      }
       target.classList.add('active');
       this.lastActiveEl = target;
       this.currentActiveEl = target;
@@ -60,6 +59,7 @@ export class NavbarComponent implements AfterViewInit, AfterViewChecked {
       this.activeBox.nativeElement.style.left = target.offsetLeft + 'px';
       this.activeBox.nativeElement.style.width = target.offsetWidth + 'px';
       this.activeBox.nativeElement.style.height = target.offsetHeight + 'px';
+      this.activeBox.nativeElement.style.opacity = 1;
     }
   }
 
