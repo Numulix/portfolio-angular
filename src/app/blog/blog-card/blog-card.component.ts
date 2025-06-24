1c6592961f9bbf46b41632f5273e06c98cf945e8
Numulix <jbabic999@gmail.com>
2025-06-24 16:36:27 +0200

    fix(blog-card): add routerLink to blog card

--- src/app/blog/blog-card/blog-card.component.ts
+++ src/app/blog/blog-card/blog-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { ButtonComponent } from "../../buttons/primary-button/button.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
     selector: "app-blog-card",
     templateUrl: "./blog-card.component.html",
     standalone: true,
-    imports: [ButtonComponent]
+    imports: [ButtonComponent, RouterModule]
 })
 export class BlogCardComponent {
     @Input() slug: string = "";
