fc52e5f50d1d58ff6c1f9742d005adc689de17f2
Numulix <jbabic999@gmail.com>
2025-07-10 21:35:55 +0200

    chore: add a11y attributes wherever possible

--- src/consts/socials.ts
+++ src/consts/socials.ts
@@ -2,22 +2,26 @@ interface SocialItem {
     href: string;
     icon: string;
     alt: string;
+    ariaLabel: string;
 }
 
 export const socials: SocialItem[] = [
     {
         href: "https://github.com/Numulix",
         icon: "/socials/github.svg",
-        alt: "GitHub"
+        alt: "GitHub",
+        ariaLabel: "Follow me on GitHub"
     },
     {
         href: "https://www.linkedin.com/in/jovan-babi%C4%87-56530b179/",
         icon: "/socials/linkedin.svg",
-        alt: "LinkedIn"
+        alt: "LinkedIn",
+        ariaLabel: "Connect with me on LinkedIn"
     },
     {
         href: "https://www.instagram.com/jbabic_numulix/",
         icon: "/socials/instagram.svg",
-        alt: "Instagram"
+        alt: "Instagram",
+        ariaLabel: "Follow me on Instagram"
     }
 ]
